81534f38837236301bee6b3108cb6fd76de7903d
yjq635 <yjq635@gmail.com>
2015-08-21 12:30:06 +0800

    hide id

--- config/sqlmap.py
+++ config/sqlmap.py
@@ -3,5 +3,5 @@ mysql_processlist_hide = ["cmdline","SQL"]
 mysql_processlist_sql = "SELECT `id`,`USER`,`HOST`,`db`,`command`,`TIME`,`state`,`info` `SQL`,`pid`,`process`,`appname`,`cmdline` FROM `PROCESSLIST`"
 projects_2_branches_sql = "SELECT `id`,`USER`,`HOST`,`db`,`command`,`TIME`,`state`,`info` `SQL`,`pid`,`process`,`appname`,`cmdline` FROM `PROCESSLIST`"
 
-ecsinfo_hide = ["bindWidth","internetIp","isWin"]
+ecsinfo_hide = ["id","bindWidth","internetIp","isWin"]
 ecsinfo_sql = "SELECT * FROM `ecsinfo` "
